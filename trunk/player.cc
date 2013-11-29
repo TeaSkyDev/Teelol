c30b8bcaca449e0e9a0c9f9fe8b088f2a53d9c63
Guillaume Gas <guillaume@gas28.net>
2013-11-29 16:18:52 +0100

    Classe Player complétée avec fonction set et get

--- trunk/player.cc
+++ trunk/player.cc
@@ -1,5 +1,21 @@
 #include <iostream>
 #include "player.hh"
 
+using namespace std;
+
 Player::Player() : m_nick(nick) {}
 
+int Player::get_x() {
+	return m_x;
+}
+int Player::get_y() {
+	return m_y;
+}
+string Player::get_nick() {
+	return m_nick;
+}
+
+void Player::set_position(unsigned int x, unsigned int y) {
+	m_x = x;
+	m_y = y;
+}
