af49847a899c8313a86bc5a1bea23f1982e80ccc
emile cadorel <ecadorel@gmail.com>
2013-11-29 15:51:19 +0100

    ajout du connect au moveOk

--- trunk/client.cc
+++ trunk/client.cc
@@ -11,8 +11,11 @@ namespace Teelol{
   struct session_on_client: public session<my_proto>{
     
     session_on_client(socket &io): session<my_proto>(io){
+      proto.moveOk.sig_recv.connect(EZMETHOD(this, do_moveOk));
     }
     
+    void do_moveOk(int a ,int b){
+    }
     
   };
 };
