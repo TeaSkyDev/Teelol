88d48408e2dc029237ffd0e5c99a7f3715aab7df
emile cadorel <ecadorel@gmail.com>
2013-11-29 15:58:20 +0100

    ajout du nouveaux signal

--- trunk/proto.hh
+++ trunk/proto.hh
@@ -8,11 +8,12 @@ namespace Teelol{
   struct my_proto: public protocol<>{
     message <0, void(int, int)> move;  //requete client
     message <1, void(int, int)> moveOk;//reponse serveur
-    message <2, void(string)>   nick;
-    message <3, void(string)>   err;
-    message <4, void()>         ok;
-    message <5, void(string)>   joined;
-    message <6, void(string)>   left;
+    message <2, void(int, int, string)> moved;
+    message <3, void(string)>   nick;
+    message <4, void(string)>   err;
+    message <5, void()>         ok;
+    message <6, void(string)>   joined;
+    message <7, void(string)>   left;
 
     my_proto(socket &io): protocol<>(io), move(this), moveOk(this),
 			  nick(this),     err(this),  ok(this),
