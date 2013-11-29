a1c41e6fc7e7c8f9c81a4e43c75b449c39a9906d
emile cadorel <ecadorel@gmail.com>
2013-11-29 15:38:20 +0100

    remplissage d'un protocol

--- trunk/proto.hh
+++ trunk/proto.hh
@@ -1,9 +1,26 @@
 #include "netez.hh"
-
+#include <iostream>
 namespace Teelol{
 
+  using namespace std;
+  using namespace netez;
+
   struct my_proto: public protocol<>{
-  }
+    message <0, void(int, int)> move;  //requete client
+    message <1, void(int, int)> moveOk;//reponse serveur
+    message <2, void(string)>   nick;
+    message <3, void(string)>   err;
+    message <4, void()>         ok;
+    message <5, void(string)>   joined;
+    message <6, void(string)>   left;
+
+    my_proto(socket &io): protocol<>(io), move(this), moveOk(this),
+			  nick(this),     err(this),  ok(this),
+			  joined(this),   left(this)
+    {}
+    
+
+  };
 
 
 
