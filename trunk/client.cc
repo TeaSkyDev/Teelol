7dc556093c21e9efca67bcac982b9530067fc3cf
Guillaume Gas <guillaume@gas28.net>
2013-12-03 15:36:41 +0100

    Fonctions do_moveOk, do_moved, do_err et do_ok codées.

--- trunk/client.cc
+++ trunk/client.cc
@@ -8,14 +8,22 @@ namespace Teelol {
   using namespace std;
   using namespace netez;
   
+  enum state_t {
+      STARTING,
+      STARTED
+  };
+
   struct session_on_client: public session<my_proto>{
   
     vector<Player*> players;
     Player * player;
 
+    state_t state;
+
     session_on_client(socket &io): session<my_proto>(io){
 
       player = new Player("nameless");
+      state  = STARTING;
 
       proto.moveOk.sig_recv.connect(EZMETHOD(this, do_moveOk));
       proto.moved.sig_recv.connect(EZMETHOD(this, do_moved));
@@ -26,18 +34,27 @@ namespace Teelol {
     }
     
     void do_moveOk(int x ,int y){
+      player->set_position(x, y);
     }
 
     void do_moved(int x, int y, string nick) {
-
+      for(int i = 0; i < players.size(); i++) {
+        if(players[i]->get_nick() == nick) {
+          players[i]->set_position(x, y);
+        }
+      } 
     }
 
     void do_err(string err) {
-
+      cout << "System : " << err << endl;
     }
 
     void do_ok() {
+      if(state = STARTING) {
+        state = STARTED;
+      }
 
+      cout << "System : success" << endl;
     }
 
     void do_joined(string nick) {
