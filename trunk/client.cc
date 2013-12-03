16129f70ad4fbc6a9244e251080fb94f19b27bf6
Guillaume Gas <guillaume@gas28.net>
2013-12-03 15:10:21 +0100

    Ajout fonctions du clients (pas encore codé)

--- trunk/client.cc
+++ trunk/client.cc
@@ -1,25 +1,55 @@
+#include <vector>
+
 #include "proto.hh"
+#include "player.hh"
 
-
-namespace Teelol{
+namespace Teelol {
 
   using namespace std;
   using namespace netez;
   
-  
-  
   struct session_on_client: public session<my_proto>{
-    
+  
+    vector<Player*> players;
+    Player * player;
+
     session_on_client(socket &io): session<my_proto>(io){
+
+      player = new Player("nameless");
+
       proto.moveOk.sig_recv.connect(EZMETHOD(this, do_moveOk));
+      proto.moved.sig_recv.connect(EZMETHOD(this, do_moved));
+      proto.err.sig_recv.connect(EZMETHOD(this, do_err));
+      proto.ok.sig_recv.connect(EZMETHOD(this, do_ok));
+      proto.joined.sig_recv.connect(EZMETHOD(this, do_joined));
+      proto.left.sig_recv.connect(EZMETHOD(this, do_left));
     }
     
-    void do_moveOk(int a ,int b){
+    void do_moveOk(int x ,int y){
+    }
+
+    void do_moved(int x, int y, string nick) {
+
+    }
+
+    void do_err(string err) {
+
+    }
+
+    void do_ok() {
+
+    }
+
+    void do_joined(string nick) {
+
+    }
+
+    void do_left(string nick) {
+
     }
     
   };
 };
-
 
 int main(int argc, char ** argv){
   netez::client<Teelol::session_on_client> client(argc,argv);
