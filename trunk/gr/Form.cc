378588cdbaf02dc8ce8933326cc7699db83dae40
emile cadorel <ecadorel@gmail.com>
2013-12-06 15:12:49 +0100

    ajout de la classe Form avec les methodes basique applicable a chaque objet (geter et seter)

---
+++ trunk/gr/Form.cc
@@ -0,0 +1,12 @@
+#include "Form.hh"
+
+
+void Form::set_image(string img){
+  m_surf = IMG_Load(img);
+}
+
+
+int Form::get_l(){ return m_l;}
+int Form::get_h(){ return m_h;}
+int Form::get_x(){ return m_x;}
+int Form::get_y(){ return m_y;}
