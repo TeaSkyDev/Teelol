d516c39dd940e5dcc40b12f95541894fc61a48ed
emile cadorel <ecadorel@gmail.com>
2013-12-10 14:46:44 +0100

    modification du main

--- trunk/gr/main.cc
+++ trunk/gr/main.cc
@@ -6,7 +6,8 @@ void routine(){
   Ecran e(300,300);
   string action;
   Character r("",10,10,10,10,&e);
-  
+  Character r2("",10,290,10,300,&e);
+  r.add_obstacle(r2);
   SDL_Event ev;
   while(1){
     SDL_PollEvent(&ev);
@@ -29,7 +30,7 @@ void routine(){
     r.show();
     
     r2.show();
-    r3.show();
+    // r3.show();
     e.clean();
     SDL_Delay(50);
   }
