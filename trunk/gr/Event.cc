2b77dee9e34ee82f08d9039c2c36c18211fe132a
emile cadorel <ecadorel@gmail.com>
2013-12-10 15:16:19 +0100

    ajout de la classe Event

---
+++ trunk/gr/Event.cc
@@ -0,0 +1,41 @@
+#include "Event.hh"
+
+Event::Event(){
+  m_mapping[LEFT] = SDLK_LEFT;
+  m_mapping[RIGHT] = SDLK_RIGHT;
+  m_mapping[JUMP] = SDLK_SPACE;
+  m_mapping[QUIT] = SDLK_ESCAPE;
+  m_in.m_key[m_mapping[JUMP]] = 0;
+  m_in.m_key[m_mapping[LEFT]] = 0;
+  m_in.m_key[m_mapping[RIGHT]] = 0;
+  m_in.m_key[m_mapping[QUIT]] = 0;
+}
+
+
+void Event::change_key(Mapp m, SDLKey k){
+  m_mapping[m] = k;
+  m_in.m_key[k] = 0;
+}
+
+
+void Event::UpdateEvent(){
+  while(SDL_PollEvent(&m_event)){
+    if(m_event.type == SDL_KEYDOWN){
+      m_in.m_key[m_event.key.keysym.sym] = 1;
+    }
+    if(m_event.type == SDL_KEYUP){
+      m_in.m_key[m_event.key.keysym.sym] = 0;
+    }  
+  }
+}
+
+bool Event::key_pressed(Mapp m){
+  return m_in.m_key[m_mapping[m]] == 1;
+}
+
+
+void Event::reset_pressed(Mapp m){
+  m_in.m_key[m_mapping[m]] = 0;
+}
+
+
--- trunk/gr/main.cc
+++ trunk/gr/main.cc
@@ -1,37 +1,34 @@
 #include "Ecran.hh"
 #include "Character.hh"
-
+#include "Event.hh"
 
 void routine(){
-  Ecran e(300,300);
+  Ecran sc(300,300);
   string action;
-  Character r("",10,10,10,10,&e);
-  Character r2("",10,290,10,300,&e);
+  Character r("",10,10,10,10,&sc);
+  Character r2("",10,290,300,10,&sc);
   r.add_obstacle(r2);
-  SDL_Event ev;
+  Event e;
   while(1){
-    SDL_PollEvent(&ev);
-    if(ev.type == SDL_KEYDOWN){
-      if(ev.key.keysym.sym == SDLK_LEFT)
+    e.UpdateEvent();
+    if(e.key_pressed(LEFT))
 	r.move_left();
-      if(ev.key.keysym.sym == SDLK_RIGHT)
+    else if(e.key_pressed(RIGHT))
 	r.move_right();
-      if(ev.key.keysym.sym == SDLK_SPACE)
-	r.jump();
-      if(ev.key.keysym.sym == SDLK_DOWN)
-	{
-	  r.stop_x();
-	  r.stop_y();
-	}
-      if(ev.key.keysym.sym == SDLK_ESCAPE)
-	break;
+    else if(!e.key_pressed(LEFT) && !e.key_pressed(RIGHT))
+      r.stop_x();
+    if(e.key_pressed(JUMP)){
+      r.jump();
+      e.reset_pressed(JUMP);
     }
+    if(e.key_pressed(QUIT))
+      break;
     r.pass_row();
     r.show();
     
     r2.show();
     // r3.show();
-    e.clean();
+    sc.clean();
     SDL_Delay(50);
   }
 
