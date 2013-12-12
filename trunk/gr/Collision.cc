b2f16f3fa7c8a70a91090ab78af902cc02e993e9
emile cadorel <ecadorel@gmail.com>
2013-12-12 15:08:18 +0100

    modification de la collision

--- trunk/gr/Collision.cc
+++ trunk/gr/Collision.cc
@@ -5,7 +5,7 @@ Collision::Collision(Form * f, vector <Form*> tab){
   m_direction.col_y = NONE;
    for(int i = 0 ; i < tab.size() ; i++){
       direction_t t = is_inside(f, tab[i]);
-      if(t.col_y != NONE) {
+      if(t.col_y != NONE && m_direction.col_y != SOUTH) {
         m_direction.col_y = t.col_y;  
       }
       if(t.col_x != NONE) {
@@ -34,8 +34,7 @@ bool Collision::is_inside(int x,int y, Form *t){
 
 
 direction_t Collision::is_inside(Form *f, Form * t) {
-
-  Form f1(f->get_x() + 3, f->get_y(), 3, f->get_l() - 6); //nord
+   Form f1(f->get_x() + 3, f->get_y(), 3, f->get_l() - 6); //nord
   Form f2(f->get_x() + 3, f->get_y() + f->get_h() - 3, 3, f->get_l() -6); //sud
   Form f3(f->get_x() +4 + f->get_l() - 3, f->get_y() + 3, f->get_h() - 6, 3); //est
   Form f4(f->get_x() -4, f->get_y() + 3, 3, f->get_h() - 6); //ouest
@@ -45,6 +44,7 @@ direction_t Collision::is_inside(Form *f, Form * t) {
   if(col(&f1, t)) {
     dir.col_y = NORTH;
   } else if(col(&f2, t)) {
+
     dir.col_y = SOUTH;
   } else {
     dir.col_y = NONE;
@@ -68,7 +68,7 @@ bool Collision::col(Form * f, Form * t) {
   bool col_south = f->get_y() >= (t->get_y() + t->get_h());
   bool col_east  = f->get_x() >= (t->get_x() + t->get_l());
   bool col_west  = (f->get_x() + f->get_l()) <= t->get_x();
-
+  
   return !(col_north || col_south || col_east || col_west);
 
 }
