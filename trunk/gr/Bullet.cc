39175d8725e4fdf6425536331509e2b818ea3cab
emile cadorel <ecadorel@gmail.com>
2013-12-17 13:22:39 +0100

    ajout de la methode pass_row() qui permet de bouger la balle lors d'un tour de boucle

--- trunk/gr/Bullet.cc
+++ trunk/gr/Bullet.cc
@@ -9,3 +9,40 @@ Bullet::Bullet(int x, int y, int h, int l, int dmg, string img):Form(x,y,h,l), m
 int Bullet::get_dmg(){
   return m_dmg;
 }
+
+void Bullet::pass_row(){
+  bool continuer = true;
+  for(int i = 0 ; i < abs(m_speed.m_y) ; i++){
+    direction_t d = collide();
+    if(d.col_y == NORTH || d.col_y == SOUTH){
+      explode();
+      continuer = false;
+    }
+    if(m_speed.m_y > 0)
+      m_y++;
+    else m_y--;
+  }
+  for (int i = 0 ; i < abs(m_speed.m_x) && continuer; i++){
+    direction_t d = collide();
+    if(d.col_x == EAST || d.col_x == WEST){
+      explode();
+    }
+    if(m_speed.m_x > 0)
+      m_x++;
+    else
+      m_x --;
+  }
+}
+
+void Bullet::explode(){
+  set_image("img/explosion.png");
+}
+
+void Bullet::add_obstacle(Form * f){
+  m_obstacle.push_back(f);
+}
+
+direction_t Bullet::collide(){
+  Collision c(this, m_obstacle);
+  return c.get_direction();
+}
--- trunk/gr/Bullet.hh
+++ trunk/gr/Bullet.hh
@@ -2,17 +2,22 @@
 #define _BULLET
 
 #include "Form.hh"
+#include "Collision.hh"
+#include <vector>
+#include <math.h>
 
 class Bullet: public Form{
 public:
   Bullet(int x, int y, int h, int l, int dmg, string img);
   int get_dmg();
-
-  
+  void pass_row();
+  void explode();
+  void add_obstacle(Form * f);
+  direction_t collide();
 
 private:
   int m_dmg;
-
+  vector<Form *> m_obstacle;
 };
 
 
