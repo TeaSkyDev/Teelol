c70e3791e1e0be8fb8f74bca3abda8385b69ad1c
emile cadorel <ecadorel@gmail.com>
2014-01-13 13:17:15 +0100

    modification du proto

--- trunk/proto.hh
+++ trunk/proto.hh
@@ -19,14 +19,14 @@ namespace Teelol{
     message <6, void(string)>                     joined;        //Informe les joueurs d'un arrivant
     message <7, void(string)>                     left;          //Informe les joueurs d'un départ
     message <8, void()>                           quit;          //Signal au serveur de la déconnexion du client
-    message <9, void(string)>                     okNick ;        //réponse positive à la demande de pseudo d'un joueur
-    message <10, void(int, int)>                  showMissile;   //indique la position la position d'un missile
+    message <9, void(string)>                     okNick ;       //réponse positive à la demande de pseudo d'un joueur
+    message <10, void(string, int,int)>           addBullet;     //serveur envoi une bullet au client
     message <11, void(int, int , int , int)>      shoot;         //client indique qu'il a tiré
-    message <12, void(int, int, int ,int, int)>   addObstacle; //serveur envoi un obstacle a afficher au client
-    message <13, void(int)>                       rotate;      //le client envoi qu'il tourne son arme
-    message <14 , void(int, string)>              rotated;     //serveur envoi qu'un client a tourne son arme
+    message <12, void(int, int, int ,int, int)>   addObstacle;   //serveur envoi un obstacle a afficher au client
+    message <13, void(int)>                       rotate;        //le client envoi qu'il tourne son arme
+    message <14 , void(int, string)>              rotated;       //serveur envoi qu'un client a tourne son arme
     message <15, void(int)>                       nbAmmo;        //serveur envoi le nombre de munition restante au client
-    message <16, void(int, int)>                  showExplosion; //indique la position a laquelle le client doit aficher une explosion 
+    message <16, void(string, int,int)>           bulletMoved;   //serveur envoi le deplacement d'un bullet
     message <17, void(int)>                       hurt;        //serveur envoi au client qu'il s'est fait mal
     message <18, void(string)>                    hurted;      //serveur envoi qu'un client s'est fait mal
     message <19, void(int)>                       health;      //serveur envoi au client qu'il a gagne de la vie
@@ -35,15 +35,16 @@ namespace Teelol{
     message <22 , void(int)>                      showItem;    //serveur envoi qu'il faut afficher l'item
     message <23, void()>                          winPoint;    //serveur envoi au client qu'il a gagner des points
     message <24, void()>                          loosePoint;  //serveur envoi au client qu'il a perdu des points
+    message <25, void(string)>                    explode;     //serveur envoi l'explosion d'un bullet
 
     my_proto(socket &io): protocol<>(io), move(this), moveOk(this),
 			  nick(this),     err(this),  ok(this),
 			  joined(this),   left(this), moved(this), quit(this), 
-			  okNick(this), addObstacle(this), showMissile(this), 
+			  okNick(this), addObstacle(this), addBullet(this), 
 			  shoot(this), rotate(this), rotated(this), nbAmmo(this),
-			  showExplosion(this), hurt(this), hurted(this), 
+			  bulletMoved(this), hurt(this), hurted(this), 
 			  health(this), addItem(this), hideItem(this), showItem(this),
-			  winPoint(this), loosePoint(this)
+			  winPoint(this), loosePoint(this), explode(this)
 			  
     {}
     
