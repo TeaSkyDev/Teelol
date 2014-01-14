c9c76c83e77a7f0efbb592c09503d8bd5faef0ab
emile cadorel <ecadorel@gmail.com>
2014-01-14 14:49:13 +0100

    modification de la release

--- REL_1.0/proto.hh
+++ REL_1.0/proto.hh
@@ -20,30 +20,29 @@ namespace Teelol{
     message <7, void(string)>                     left;          //Informe les joueurs d'un départ
     message <8, void()>                           quit;          //Signal au serveur de la déconnexion du client
     message <9, void(string)>                     okNick ;       //réponse positive à la demande de pseudo d'un joueur
-    message <10, void(string, int,int)>           addBullet;     //serveur envoi une bullet au client
+    message <10, void(string, int,int,int,int)>   addBullet;     //serveur envoi une bullet au client
     message <11, void(int, int , int , int)>      shoot;         //client indique qu'il a tiré
     message <12, void(int, int, int ,int, int)>   addObstacle;   //serveur envoi un obstacle a afficher au client
     message <13, void(int)>                       rotate;        //le client envoi qu'il tourne son arme
     message <14 , void(int, string)>              rotated;       //serveur envoi qu'un client a tourne son arme
     message <15, void(int)>                       nbAmmo;        //serveur envoi le nombre de munition restante au client
-    message <16, void(string, int,int)>           bulletMoved;   //serveur envoi le deplacement d'un bullet
-    message <17, void(int)>                       hurt;        //serveur envoi au client qu'il s'est fait mal
-    message <18, void(string)>                    hurted;      //serveur envoi qu'un client s'est fait mal
-    message <19, void(int)>                       health;      //serveur envoi au client qu'il a gagne de la vie
-    message <20, void(int, int, int, int)>        addItem;     //serveur envoi au client un item a afficher
-    message <21, void(int)>                       hideItem;    //serveur envoi qu'il faut cacher l'item
-    message <22 , void(int)>                      showItem;    //serveur envoi qu'il faut afficher l'item
-    message <23, void()>                          winPoint;    //serveur envoi au client qu'il a gagner des points
-    message <24, void()>                          loosePoint;  //serveur envoi au client qu'il a perdu des points
-    message <25, void(string)>                    explode;     //serveur envoi l'explosion d'un bullet
+    message <16, void(int)>                       hurt;        //serveur envoi au client qu'il s'est fait mal
+    message <17, void(string)>                    hurted;      //serveur envoi qu'un client s'est fait mal
+    message <18, void(int)>                       health;      //serveur envoi au client qu'il a gagne de la vie
+    message <19, void(int, int, int, int)>        addItem;     //serveur envoi au client un item a afficher
+    message <20, void(int)>                       hideItem;    //serveur envoi qu'il faut cacher l'item
+    message <21 , void(int)>                      showItem;    //serveur envoi qu'il faut afficher l'item
+    message <22, void()>                          winPoint;    //serveur envoi au client qu'il a gagner des points
+    message <23, void()>                          loosePoint;  //serveur envoi au client qu'il a perdu des points
+    message <24, void(string)>                    explode;     //serveur envoi l'explosion d'un bullet
 
     my_proto(socket &io): protocol<>(io), move(this), moveOk(this),
 			  nick(this),     err(this),  ok(this),
 			  joined(this),   left(this), moved(this), quit(this), 
 			  okNick(this), addObstacle(this), addBullet(this), 
 			  shoot(this), rotate(this), rotated(this), nbAmmo(this),
-			  bulletMoved(this), hurt(this), hurted(this), 
-			  health(this), addItem(this), hideItem(this), showItem(this),
+			  hurt(this), hurted(this), health(this), 
+			  addItem(this), hideItem(this), showItem(this),
 			  winPoint(this), loosePoint(this), explode(this)
 			  
     {}
